33ea2587bf4bb80b40629911a323786fcc9002bf
sanchan <13522955396@163.com>
2017-05-24 11:21:17 +0800

    Initial commit

---
+++ src/main/java/com/coderknock/sanchan/factory/simplefactory/SimpleFactoryMain.java
@@ -0,0 +1,15 @@
+package com.coderknock.sanchan.factory.simplefactory;
+
+/**
+ * <p></p>
+ *
+ * @author 三产
+ * @version 1.0
+ * @date 2017-05-24
+ * @QQGroup 213732117
+ * @website http://www.coderknock.com
+ * @copyright Copyright 2017 拿客 coderknock.com  All rights reserved.
+ * @since JDK 1.8
+ */
+public class SimpleFactoryMain {
+}
